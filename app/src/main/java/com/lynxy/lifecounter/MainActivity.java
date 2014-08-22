782d8b5d28ea96e28724db6258833f005c43b2fc
Lynxfox <cime.anja@gmail.com>
2014-08-22 15:44:59 +0200

    first commit

--- app/src/main/java/com/lynxy/lifecounter/MainActivity.java
+++ app/src/main/java/com/lynxy/lifecounter/MainActivity.java
@@ -1,19 +1,29 @@
 package com.lynxy.lifecounter;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Typeface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-	private int startingLife = 20;
+    private static final String SAVED_STATE_PLAYER_1_LIFE = "savedStatePlayer1Life";
+    private static final String SAVED_STATE_PLAYER_1_POISON = "savedStatePlayer1Poison";
+    private static final String SAVED_STATE_PLAYER_2_LIFE = "savedStatePlayer2Life";
+    private static final String SAVED_STATE_PLAYER_2_POISON = "savedStatePlayer2Poison";
+
+    //settings
+	private int startingLife= 20;
+    //counters
 	private int player1Life = startingLife;
 	private int player2Life = startingLife;
 	private int player1Poison = 0;
@@ -22,151 +32,106 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
 
-		getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            Window w = getWindow(); // in Activity's onCreate() for instance
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION, WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            w.setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR_OVERLAY);
+        ActionBar actionBar = getActionBar();
+        actionBar.setBackgroundDrawable(new ColorDrawable(Color.parseColor("#00000000")));
+        actionBar.setDisplayShowHomeEnabled(false);
+        setContentView(R.layout.activity_main);
 
-		if (savedInstanceState != null) {
-			onRestoreInstanceState(savedInstanceState);
-		}
+        if (savedInstanceState != null) {
+            player1Life = savedInstanceState.getInt(SAVED_STATE_PLAYER_1_LIFE);
+            player1Poison = savedInstanceState.getInt(SAVED_STATE_PLAYER_1_POISON);
+            player2Life = savedInstanceState.getInt(SAVED_STATE_PLAYER_2_LIFE);
+            player2Poison = savedInstanceState.getInt(SAVED_STATE_PLAYER_2_POISON);
+            refreshPlayer1Life();
+            refreshPlayer2Life();
+        } else {
+            // We need to load it once here to be able to use it in resetLife() even if we are
+            // loading it again in onStart()
+           // startingLife = SharedPrefsConfig.getInt(this, SharedPrefsConfig.STARTING_LIFE, 20);
+            resetLife();
+        }
 
-		TextView tV1 = (TextView) findViewById(R.id.textView1);
-		TextView tV2 = (TextView) findViewById(R.id.textView2);
-		Button p1m5 = (Button) findViewById(R.id.p1minus5);
-		Button p1p1 = (Button) findViewById(R.id.p1plus1);
-		Button p1p5 = (Button) findViewById(R.id.p1plus5);
-		Button p2m5 = (Button) findViewById(R.id.p2minus5);
-		Button p2p1 = (Button) findViewById(R.id.p2plus1);
-		Button p2p5 = (Button) findViewById(R.id.p2plus5);
-
-		Typeface typeFace = Typeface.createFromAsset(getAssets(),
-				"fonts/Roboto-Thin.ttf");
-		tV1.setTypeface(typeFace);
-		tV2.setTypeface(typeFace);
-		p1m5.setTypeface(typeFace);
-		p1p1.setTypeface(typeFace);
-		p1p5.setTypeface(typeFace);
-		p2m5.setTypeface(typeFace);
-		p2p1.setTypeface(typeFace);
-		p2p5.setTypeface(typeFace);
-
-		findViewById(R.id.textView1).setOnClickListener(
+		findViewById(R.id.player1_life_add).setOnClickListener(
 				new View.OnClickListener() {
 
 					@Override
 					public void onClick(View v) {
-						player1Life--;
-						if (player1Life < 0) {
-							player1Life = 0;
-						}
+						player1Life++;
 						refreshPlayer1Life();
-
-					}
+                    }
 				});
 
-		findViewById(R.id.textView2).setOnClickListener(
-				new View.OnClickListener() {
+        findViewById(R.id.player1_life_remove).setOnClickListener(
+                new View.OnClickListener() {
 
-					@Override
-					public void onClick(View v) {
-						player2Life--;
-						if (player2Life < 0) {
-							player2Life = 0;
-						}
-						refreshPlayer2Life();
-					}
-				});
+                    @Override
+                    public void onClick(View v) {
+                        player1Life--;
+                        refreshPlayer1Life();
+                    }
+                });
 
-		findViewById(R.id.p1poison).setOnClickListener(
-				new View.OnClickListener() {
+        findViewById(R.id.player2_life_add).setOnClickListener(
+                new View.OnClickListener() {
 
-					@Override
-					public void onClick(View v) {
-						player1Poison++;
+                    @Override
+                    public void onClick(View v) {
+                        player2Life++;
+                        refreshPlayer2Life();
+                    }
+                });
 
-						refreshPlayer1Life();
-					}
-				});
-		findViewById(R.id.p2poison).setOnClickListener(
-				new View.OnClickListener() {
+        findViewById(R.id.player2_life_remove).setOnClickListener(
+                new View.OnClickListener() {
 
-					@Override
-					public void onClick(View v) {
-						player2Poison++;
+                    @Override
+                    public void onClick(View v) {
+                        player2Life--;
+                        refreshPlayer2Life();
+                    }
+                });
 
-						refreshPlayer2Life();
-					}
-				});
-		findViewById(R.id.p1minus5).setOnClickListener(
-				new View.OnClickListener() {
+        findViewById(R.id.reset).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        resetLife();
+                    }
+                });
 
-					@Override
-					public void onClick(View v) {
-						player1Life -= 5;
-						if (player1Life < 0) {
-							player1Life = 0;
-						}
-						refreshPlayer1Life();
-					}
-				});
 
-		findViewById(R.id.p2minus5).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						player2Life -= 5;
-						if (player2Life < 0) {
-							player2Life = 0;
-						}
-						refreshPlayer2Life();
-					}
-				});
-
-		findViewById(R.id.p1plus1).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						player1Life += 1;
-						refreshPlayer1Life();
-					}
-				});
-
-		findViewById(R.id.p2plus1).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						player2Life += 1;
-						refreshPlayer2Life();
-					}
-				});
-
-		findViewById(R.id.p1plus5).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						player1Life += 5;
-						refreshPlayer1Life();
-					}
-				});
-
-		findViewById(R.id.p2plus5).setOnClickListener(
-				new View.OnClickListener() {
-
-					@Override
-					public void onClick(View v) {
-						player2Life += 5;
-						refreshPlayer2Life();
-					}
-				});
-
-		refreshPlayer1Life();
+        refreshPlayer1Life();
 		refreshPlayer2Life();
 
 	}
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        refreshPlayer1Life();
+        refreshPlayer2Life();
+        //startingLife = SharedPrefsConfig.getInt(this, SharedPrefsConfig.STARTING_LIFE, 20);
+
+
+        //boolean keepScreenAwake = SharedPrefsConfig.getBoolean(this,
+         //       SharedPrefsConfig.KEEP_SCREEN_AWAKE, true);
+        //if (keepScreenAwake) {
+          //  getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //} else {
+          //  getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        //}
+    }
+
 
 	@Override
 	protected void onSaveInstanceState(Bundle savedInstanceState) {
@@ -187,83 +152,46 @@ public class MainActivity extends Activity {
 		player1Poison = savedInstanceState.getInt("Player1Poison");
 		player2Poison = savedInstanceState.getInt("Player2Poison");
 
+
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
-
-		return true;
+        return true;
 	}
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		// Handle item selection
 		switch (item.getItemId()) {
-		case R.id.action_reset:
-			player1Life = startingLife;
-			player2Life = startingLife;
-			player1Poison = 0;
-			player2Poison = 0;
-			refreshPlayer1Life();
-			refreshPlayer2Life();
-			return true;
-
-		case R.id.action_settings:
-			Intent intent = new Intent(this, SettingsActivity.class);
-			startActivity(intent);
-			return true;
-
-			// case R.id.action_2Hgiant:
-			//
-			// item.setChecked(true);
-			// startingLife = 30;
-			// player1Life = startingLife;
-			// player2Life = startingLife;
-			// player1Poison = 0;
-			// player2Poison = 0;
-			// poisonMax = 15;
-			// refreshPlayer1Life();
-			// refreshPlayer2Life();
-			//
-			// return true;
-			// case R.id.action_commander:
-			// item.setChecked(true);
-			// startingLife = 40;
-			// player1Life = startingLife;
-			// player2Life = startingLife;
-			// player1Poison = 0;
-			// player2Poison = 0;
-			// poisonMax = 10;
-			// refreshPlayer1Life();
-			// refreshPlayer2Life();
-			//
-			// return true;
-			// // case R.id.action_normal:
-			// // item.setChecked(true);
-			// // startingLife = 20;
-			// // player1Life = startingLife;
-			// // player2Life = startingLife;
-			// // player1Poison = 0;
-			// // player2Poison = 0;
-			// // poisonMax = 10;
-			// // refreshPlayer1Life();
-			// // refreshPlayer2Life();
-			//
-			// return true;
-		default:
-			return super.onOptionsItemSelected(item);
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
 		}
 	}
 
 	private void refreshPlayer1Life() {
-		final TextView textViewToChange = (TextView) findViewById(R.id.textView1);
-		textViewToChange.setText("" + player1Life + "/" + player1Poison);
+		final TextView textViewToChange = (TextView) findViewById(R.id.player1_life);
+		textViewToChange.setText("" + player1Life);
 	}
 
 	private void refreshPlayer2Life() {
-		final TextView textViewToChange2 = (TextView) findViewById(R.id.textView2);
-		textViewToChange2.setText("" + player2Life + "/" + player2Poison);
+		final TextView textViewToChange2 = (TextView) findViewById(R.id.player2_life);
+		textViewToChange2.setText("" + player2Life);
 	}
+
+    private void resetLife(){
+        player1Life = startingLife;
+        player2Life = startingLife;
+        player1Poison = 0;
+        player2Poison = 0;
+        refreshPlayer1Life();
+        refreshPlayer2Life();
+
+    }
 }
--- app/src/main/java/com/lynxy/lifecounter/SettingsActivity.java
+++ app/src/main/java/com/lynxy/lifecounter/SettingsActivity.java
@@ -1,10 +1,13 @@
 package com.lynxy.lifecounter;
 
 import android.app.ActionBar;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -13,14 +16,13 @@ public class SettingsActivity extends PreferenceActivity implements
 
 	// private Switch mFlipCounterSwitch;
 	// private Switch mPoisonCountersSwitch;
-	private TextView mStartingLifeTextView;
 
-	private Switch mKeepScreenAwakeSwitch;
+	private Switch keepScreenAwakeSwitch;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// setContentView(R.layout.preferences);
+		//setContentView(R.xml.preferences);
 
 		addPreferencesFromResource(R.xml.preferences);
 
@@ -28,44 +30,12 @@ public class SettingsActivity extends PreferenceActivity implements
 		if (actionBar != null) {
 			actionBar.setDisplayHomeAsUpEnabled(true);
 		}
-
-		bindViews();
-
-		populateViews();
-	}
-
-	private void bindViews() {
-		// mStartingLifeTextView = (TextView)
-		// findViewById(R.id.item_starting_life);
-		// mStartingLifeTextView.setOnClickListener(this);
-		//
-		// findViewById(R.id.item_keep_screen_awake).setOnClickListener(this);
-		// mKeepScreenAwakeSwitch = (Switch)
-		// findViewById(R.id.pref_screen_on_checkbox);
-
-	}
-
-	private void populateViews() {
-
-		// int startingLife = SharedPrefsConfig.getInt(this,
-		// SharedPrefsConfig.STARTING_LIFE, 20);
-		// mStartingLifeTextView.setText(getString(
-		// R.string.settings_item_starting_life_format, startingLife));
-		//
-		// mKeepScreenAwakeSwitch.setChecked(SharedPrefsConfig.getBoolean(this,
-		// SharedPrefsConfig.KEEP_SCREEN_AWAKE, true));
-	}
-
-	public void onClick(View view) {
+    }
+    public void onClick(View view) {
 
 		switch (view.getId()) {
 
-		// case R.id.item_keep_screen_awake:
-		// mKeepScreenAwakeSwitch.toggle();
-		// SharedPrefsConfig.setBoolean(this,
-		// SharedPrefsConfig.KEEP_SCREEN_AWAKE,
-		// mKeepScreenAwakeSwitch.isChecked());
-		// break;
+
 		}
 	}
 
